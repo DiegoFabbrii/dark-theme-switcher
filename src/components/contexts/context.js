641133ff3a275ed24a72c28bbfa7df6218a009de
Diego Fabbri <diegofabbri@outlook.com>
2022-03-23 17:18:37 -0300

    update

--- src/components/contexts/context.js
+++ src/components/contexts/context.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { light } from "../../styles/themes/light";
 import { dark } from "../../styles/themes/dark";
 
@@ -6,6 +6,17 @@ export const MyThemeContext = createContext();
 
 function ThemeContextProvider({ children }) {
     const [theme, setTheme] = useState(light);
+
+    useEffect(() => {
+        const currentTheme = JSON.parse(localStorage.getItem("theme"));
+        setTheme(currentTheme);
+    }, []);
+
+    useEffect(() => {
+        const saveTheme = () =>
+            localStorage.setItem("theme", JSON.stringify(theme));
+        saveTheme();
+    }, [theme]);
 
     const themeHandler = () => {
         setTheme(theme.title === "light" ? dark : light);
